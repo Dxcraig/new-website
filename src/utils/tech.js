46f837c0c3a1f764248fd58c699aaf3957d9488e
Dx_craig <steviedoe20@gmail.com>
2025-08-02 10:23:10 +0000

    fix: remove outdated technologies from the tech list

--- src/utils/tech.js
+++ src/utils/tech.js
@@ -3,17 +3,6 @@ export const technologies = [
     "HTML",
     "CSS", 
     "JavaScript",
-    "Node.js",
-    "Express",
-    "SQL",
-    "MySQL",
-    "MongoDB",
     "Git",
-    "GitHub",
-    "Python",
-    "REST APIs",
-    "Postman",
-    "JSON",
-    "Linux",
-    "Command Line"
+    "GitHub"
 ]
